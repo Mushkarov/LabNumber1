f3a09a18c1cb77f56f8606c7b9332f92d340cbe0
Mushkarov <mushkarovw@mail.ru>
2024-06-09 20:14:38 +0300

    Правка lab1

--- DeniTest/DeniTest/Program.cs
+++ DeniTest/DeniTest/Program.cs
@@ -1,23 +1,69 @@
-﻿using DeniTest;
-using MyLib;
+﻿using Lab1;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Channels;
 
-namespace DeniTest
+namespace Lab1
 {
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            var a = 5;
-            var b = 0;
-            var c = (a / b) - b;           
-            var v = b / 2 * Math.Log((Math.Pow(5, 2)) + (Math.Pow(2.35, 2)));
-            var result = Math.Acos(c) - v;
+
+
+                
+                double C = 0;
+                double A = 0;
+                double B = 0;
+           for (var i = 0; i == A; i++)
+            {
+                
+               
+                Console.WriteLine("Введите чисто А");
+                
+                var chek = double.Parse(Console.ReadLine());
+                if (chek != 0)
+                {
+                    A = chek;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод данных");
+
+                    i--;
+                }
+            }
+
+            Console.WriteLine("Введите чисто B");
+            var chek1 = double.Parse(Console.ReadLine());
+            if (chek1 != 0)
+            {
+                B = chek1;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод данных");
+
+            }
+            var chek3 = (A / B) - B;
+            if (chek3 >= -1 & chek3 <= 1)
+            {
+                C = chek3;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод данных");
+            }
+            var I = Math.Acos(C);
+            var result = I - (B / 2 * Math.Log(Math.Pow(A, 2) + Math.Pow(B, 2)));
             Console.WriteLine(result);
+            Console.ReadLine();
+            Console.Clear();
+
 
         }
+        
     }
   
 }
