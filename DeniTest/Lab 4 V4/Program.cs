97b764040f9ec2dd37a5a739feeb1a62d224421c
Mushkarov <mushkarovw@mail.ru>
2024-06-16 23:47:46 +0300

    Лабораторная работа 4 Вариант 1 от 16.06.2024

---
+++ DeniTest/Lab 4 V4/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Lab_4_V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var CounterSum = 0;
+            var СounterNull = 0;
+            var СounterNegative = 0;
+            int[] Number = new int[15];
+            
+            for (int i = 0; i <= 15; i++)
+            {
+                var temp = 0;
+                if (i < 15)
+                {
+                    Console.WriteLine($"Введите {i + 1} число");
+                    if (int.TryParse(Console.ReadLine(), out temp))
+                    {
+                        Number[i] = temp;
+                    }
+                    else
+                        Console.WriteLine("Некорректный ввод данных");
+                }   
+                
+            }
+            for (int i = 0;i < Number.Length; i++)
+            {
+                
+                if (Number[i] < 0)
+                {
+                    СounterNegative ++;
+                }
+                if (Number[i] == 0)
+                {
+                    СounterNull ++;
+                }
+                if (Number[i] > 0)
+                {
+                    CounterSum = CounterSum + Number[i];
+                }
+
+            }
+
+            Console.WriteLine("Количество отрицительных чисел = " + СounterNegative);
+            Console.WriteLine("Количество чисел раных 0 = " + СounterNull);
+            Console.WriteLine("Сумма всех положительных чисел - " + CounterSum);
+        }
+    }
+}
