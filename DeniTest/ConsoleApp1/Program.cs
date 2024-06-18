41734abec90990bab4245ea826443db185605377
Mushkarov <mushkarovw@mail.ru>
2024-06-18 21:23:51 +0300

    Лабораторная работа №5

---
+++ DeniTest/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите количество строк : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("введите количество столбцов : ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Random r = new Random();
+            int[,] mas = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    mas[i, j] = r.Next(100);
+                    Console.Write(mas[i, j] + " ");
+                }
+                Console.WriteLine();
+                {
+                    Console.ReadKey(true);
+                }
+      
+            }
+        }
+    }
+}
---
+++ DeniTest/Lab 5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Channels;
+
+namespace Lab_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+           var RandomNumbers = new Random();
+            Console.WriteLine("Введите кол-во элементов по оси Х");
+            int X = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите кол-во элементов по оси Y");
+            int Y = int.Parse(Console.ReadLine());
+            int[,] MyArray = new int[X,Y];
+            for (int i = 0; i < X; i++)
+            {
+                for (int j = 0; j < Y; j++)
+                {
+                    
+                    MyArray[i,j] = RandomNumbers.Next(-100,100);
+                    Console.Write(MyArray[i, j] + " ");
+                    if (MyArray[i,j] > 0)
+                    {
+                      sum += MyArray[i,j];
+                       
+                    }
+
+                }
+                
+                Console.WriteLine();
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
